8250e0361e31473d64819380a35babe28ab91bee
SQNOT14011\nodji <nodji.junior@squadra.com.br>
2024-03-01 19:08:02 -0300

    Correção testes unitarios

--- src/test/java/com/nodji/awards/ProducerControllerIntegrationTest.java
+++ src/test/java/com/nodji/awards/ProducerControllerIntegrationTest.java
@@ -4,43 +4,27 @@ import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import com.nodji.awards.api.ProducerController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@WebAppConfiguration
+@AutoConfigureMockMvc
 class ProducerControllerIntegrationTest {
 
+	@Autowired
 	private MockMvc mockMvc;
 
-	@Autowired
-	private ProducerController producerController;
-
-	@Autowired
-	protected WebApplicationContext wac;
-
-	@Before(value = "")
-	public void setup() throws Exception {
-		this.mockMvc = MockMvcBuilders.standaloneSetup(this.producerController).build();
-		mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
-	}
-
 	@Test
-	public void getGreatestWinnersTest() throws Exception {
+	void getGreatestWinnersTest() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/producer/interval-prizes").contentType(MediaType.APPLICATION_JSON))
 				.andExpect(MockMvcResultMatchers.status().isOk())
 				.andExpect(jsonPath("$.min.*.producer", hasItem(is("Joel Silver"))))
--- src/test/java/com/nodji/awards/StudioControllerIntegrationTest.java
+++ src/test/java/com/nodji/awards/StudioControllerIntegrationTest.java
@@ -1,46 +1,40 @@
 package com.nodji.awards;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import com.nodji.awards.api.StudioController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@WebAppConfiguration
+@AutoConfigureMockMvc
 class StudioControllerIntegrationTest {
-
+	@Autowired
 	private MockMvc mockMvc;
 
 	@Autowired
-	private StudioController studioController;
+	private StudioController controller;
 
-	@Autowired
-	protected WebApplicationContext wac;
-
-	@Before(value = "")
-	public void setup() throws Exception {
-		this.mockMvc = MockMvcBuilders.standaloneSetup(this.studioController).build();
-		mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+	@Test
+	void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 	@Test
-	public void getGreatestWinnersTest() throws Exception {
+	void getGreatestWinnersTest() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/studio/winners").contentType(MediaType.APPLICATION_JSON))
 				.andExpect(MockMvcResultMatchers.status().isOk())
 				.andExpect(jsonPath("$.*.*.name", hasItem(is("Columbia Pictures"))))
